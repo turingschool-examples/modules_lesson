7e88f1aa5a13fad195556ee39e377c07eb82d382
Richard Tillies <rtillies@gmail.com>
2021-11-03 18:02:32 -0400

    first commit

---
+++ orders/runner.rb
@@ -0,0 +1,12 @@
+# ./runner.rb
+require "./amazon_order.rb"
+require "./grubhub_order.rb"
+
+amazon = AmazonOrder.new
+grub   = GrubhubOrder.new
+
+amazon.delivery
+grub.delivery
+
+amazon.review
+grub.review
